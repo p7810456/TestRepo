c178357a4f15cee7d8a366a2eb546d940e010da7
Prabhash Kumar <Prabhash_Kumar1@AMER.DELL.COM>
2017-03-03 12:08:54 -0600

    This is the final commit for today

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ void main()
 	printf("This is the first file to be commited in git\n");
 	printf("Adding the second line in the second commit\n");
 	printf("This is the first line being added to the program\n");
+	printf("This is the final commit for today\n");
 }
 
 
